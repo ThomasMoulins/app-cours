c7fa4e50036cb7afcb590aa102065a552986a673
ThomasMoulins <13thomasmoulins@gmail.com>
2024-09-16 15:16:56 +0200

    1/4

--- src/components/ClickButton.jsx
+++ src/components/ClickButton.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react"
+
 function ClickButton() {
+  const [value, setValue] = useState(0);
+
   return (
-    <button>Cliquer</button>
+    <>
+      <h1>{value}</h1>
+      <button onClick={() => setValue(value + 1)}>+</button>
+      {value > 0 && <button onClick={() => setValue(value - 1)}>-</button>}
+      <button onClick={() => setValue(0)}>RESET</button>
+    </>
   )
 }
 
