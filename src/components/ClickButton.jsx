85d444ca71a7990a86b3a842a8c55f6fb6dcfd68
ThomasMoulins <13thomasmoulins@gmail.com>
2024-09-16 15:21:40 +0200

    2/4

--- src/components/ClickButton.jsx
+++ src/components/ClickButton.jsx
@@ -6,8 +6,9 @@ function ClickButton() {
   return (
     <>
       <h1>{value}</h1>
+      {value >= 10 && <h2>BOOOOMM!!!!!!!!!!!!!</h2>}
       <button onClick={() => setValue(value + 1)}>+</button>
-      {value > 0 && <button onClick={() => setValue(value - 1)}>-</button>}
+      <button onClick={() => setValue(value - 1)} disabled={value <= 0}>-</button>
       <button onClick={() => setValue(0)}>RESET</button>
     </>
   )
