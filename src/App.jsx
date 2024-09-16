26bf6a02a28530e5b07693188f35792dc4fef34a
ThomasMoulins <13thomasmoulins@gmail.com>
2024-09-16 12:42:56 +0200

    Create branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import './App.css'
 import Ville from './components/Ville';
 import ClickButton from './components/ClickButton';
-import Student from './components/Student';
-import Logo from './assets/react.svg'
-import Avatar from '../public/vite.svg'
 
 function App() {
   let name = 'Jean';
@@ -16,8 +13,6 @@ function App() {
       <div>
         <Ville />
         <ClickButton />
-        <Student name='Jean' lastName='pasmoi' avatar={ Logo } campus='RI7' devLanguage='Flutter'/>
-        <Student name='jiji' lastName='here' avatar={ Avatar } campus='Nextech' devLanguage='React.js' isFromRi7/>
       </div>
     </>
   )
