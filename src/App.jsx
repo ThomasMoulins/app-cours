a88ee40874128493039e97d21cfe18d38b0c7724
ThomasMoulins <13thomasmoulins@gmail.com>
2024-09-16 11:58:36 +0200

    TD2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import Ville from './components/Ville';
 import ClickButton from './components/ClickButton';
+import Student from './components/Student';
+import Logo from './assets/react.svg'
+import Avatar from '../public/vite.svg'
 
 function App() {
-  let name = 'Thomas';
+  let name = 'Jean';
 
   return (
     <>
@@ -13,6 +16,8 @@ function App() {
       <div>
         <Ville />
         <ClickButton />
+        <Student name='Jean' lastName='pasmoi' avatar={ Logo } campus='RI7' devLanguage='Flutter'/>
+        <Student name='jiji' lastName='here' avatar={ Avatar } campus='Nextech' devLanguage='React.js' isFromRi7/>
       </div>
     </>
   )
