1ae603cbca93ac38544d36a062eded576e7fba38
ThomasMoulins <13thomasmoulins@gmail.com>
2024-09-17 14:01:09 +0200

    Fenetre modal

--- src/components/Formulaire.jsx
+++ src/components/Formulaire.jsx
@@ -1,117 +1,128 @@
 import { useState } from "react";
 import Input from "./Input";
+import Modal from "./Modal/Modal";
 
 const Formulaire = () => {
-
   const [formData, setFormData] = useState({
-        civility: '',
-        name: '',
-        lastname: '',
-        email: '',
-        birthdate: '',
-        cp: '',
-        ville: '',
-      });
+    civility: "",
+    name: "",
+    lastname: "",
+    email: "",
+    birthdate: "",
+    cp: "",
+    ville: "",
+  });
 
-      const handleChange = (e) => {
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-        });
-      };
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const [showModal, setShowModal] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
 
   function submitForm(event) {
     event.preventDefault();
-    setShowModal(true); 
+    setModalOpen(true);
   }
 
   function closeModal() {
-    setShowModal(false);
+    setModalOpen(false);
   }
 
   return (
     <>
       <form onSubmit={submitForm}>
-        <div>
-          <label htmlFor="civility">Civilité</label>
-          <input
-            type="radio"
-            name="civility"
-            value="Homme"
-            onChange={handleChange}
-          />H
-          <input
-            type="radio"
-            name="civility"
-            value="Femme"
-            onChange={handleChange}
-          />F
-          <input
-            type="radio"
-            name="civility"
-            value="Autre"
-            onChange={handleChange}
-          />Autre
-        </div>
         <Input
-            label="Prénom"
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
+          label="Homme"
+          type="radio"
+          name="civility"
+          value="Homme"
+          onChange={handleChange}
         />
         <Input
-            label="Nom"
-            type="text"
-            name="lastname"
-            value={formData.lastname}
-            onChange={handleChange}
+          label="Femme"
+          type="radio"
+          name="civility"
+          value="Femme"
+          onChange={handleChange}
         />
         <Input
-            label="Email"
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
+          label="Autre"
+          type="radio"
+          name="civility"
+          value="Autre"
+          onChange={handleChange}
         />
         <Input
-            label="Date de naissance"
-            type="date"
-            name="birthdate"
-            value={formData.birthdate}
-            onChange={handleChange}
+          label="Prenom"
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
         />
         <Input
-            label="Code postal"
-            type="text"
-            name="cp"
-            value={formData.cp}
-            onChange={handleChange}
+          label="Nom"
+          type="text"
+          name="lastname"
+          value={formData.lastname}
+          onChange={handleChange}
         />
         <Input
-            label="Ville"
-            type="text"
-            name="ville"
-            value={formData.ville}
-            onChange={handleChange}
+          label="Email"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <Input
+          label="Date de naissance"
+          type="date"
+          name="birthdate"
+          value={formData.birthdate}
+          onChange={handleChange}
+        />
+        <Input
+          label="Code postal"
+          type="text"
+          name="cp"
+          value={formData.cp}
+          onChange={handleChange}
+        />
+        <Input
+          label="Ville"
+          type="text"
+          name="ville"
+          value={formData.ville}
+          onChange={handleChange}
         />
         <button type="submit">Envoyé le formulaire</button>
       </form>
-      {showModal && (
-        <div className="modal">
-            <h2>Informations Saisies</h2>
-            <p><strong>Civilité :</strong> {formData.civility}</p>
-            <p><strong>Prénom :</strong> {formData.name}</p>
-            <p><strong>Nom :</strong> {formData.lastname}</p>
-            <p><strong>Email :</strong> {formData.email}</p>
-            <p><strong>Date de naissance :</strong> {formData.birthdate}</p>
-            <p><strong>Code postal :</strong> {formData.cp}</p>
-            <p><strong>Ville :</strong> {formData.ville}</p>
-            <button onClick={closeModal}>Fermer</button>
-        </div>
-      )}
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h2>Informations Saisies</h2>
+        <p>
+          <strong>Civilité :</strong> {formData.civility}
+        </p>
+        <p>
+          <strong>Prénom :</strong> {formData.name}
+        </p>
+        <p>
+          <strong>Nom :</strong> {formData.lastname}
+        </p>
+        <p>
+          <strong>Email :</strong> {formData.email}
+        </p>
+        <p>
+          <strong>Date de naissance :</strong> {formData.birthdate}
+        </p>
+        <p>
+          <strong>Code postal :</strong> {formData.cp}
+        </p>
+        <p>
+          <strong>Ville :</strong> {formData.ville}
+        </p>
+      </Modal>
     </>
   );
 };
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,26 @@
+import './Modal.css';
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose(); // Ferme la modale si le clic est sur l'overlay
+    }
+  };
+
+  return (
+    <div className="modal-overlay" onClick={handleOverlayClick}>
+      <div className="modal-content">
+        <button className="modal-close-button" onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
