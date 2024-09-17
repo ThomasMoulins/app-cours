fa3aeab63868bb22afea1855f992a177002e064f
ThomasMoulins <13thomasmoulins@gmail.com>
2024-09-17 12:08:45 +0200

    3/3

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 const Time = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [isBoom, setBoom] = useState(false); 
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -11,6 +12,14 @@ const Time = () => {
     return () => clearInterval(intervalId);
   }, []);
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setBoom(true);
+    }, 10000); // 10000 millisecondes = 10 secondes
+
+    return () => clearTimeout(timeoutId);
+  }, [])
+
   const formatTime = (date) => {
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
@@ -19,7 +28,10 @@ const Time = () => {
   };
 
   return (
-    <h1>{formatTime(currentTime)}</h1>
+    <>
+      <h1>{formatTime(currentTime)}</h1>
+      {isBoom && <h2>BOOM !!!</h2>}
+    </>
   );
 };
 
