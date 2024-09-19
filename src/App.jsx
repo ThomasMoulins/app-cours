2bdbd7dd77cf8188618c86702496cc996f3c3114
ThomasMoulins <13thomasmoulins@gmail.com>
2024-09-19 10:00:51 +0200

    Route correction

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import Router from './components/Router'
 function App() {
   return (
     <>
-      <header>
-        <a href='/'>Home</a>
-        <a href='/experiences'>Experiences</a>
-        <a href='/about'>About</a>
-      </header>
       <Router />
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <header>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/experiences'>Experiences</Link></li>
+          <li><Link to='/about'>About</Link></li>
+        </ul>
+      </header>
+      <div>Home</div>
+    </>
   )
 }
 
