e4ee2a99b5778173d3c1a25de1ee7ef1fbc73041
ThomasMoulins <13thomasmoulins@gmail.com>
2024-09-19 16:03:28 +0200

    DONE TD13-MOVIES

--- src/components/Film.jsx
+++ src/components/Film.jsx
@@ -1,19 +1,36 @@
+import styled from "styled-components";
 import FormatedDate from "./FormatedDate";
+
+const Ul = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 0 100px;
+`;
+
+const Li = styled.li`
+  list-style-type: none;
+  margin: 0.4rem;
+`;
+
+const Vote = styled.p`
+  color: ${({ vote }) => (vote >= 7 ? 'green' : '')};
+`;
 
 const Film = ({films}) => {
   return (
-    <ul>
+    <Ul>
       {films.slice(0, 5).map((film) => (
-        <li key={film.id}>
+        <Li key={film.id}>
             <img src={`https://image.tmdb.org/t/p/w300/${film.poster_path}`}/>
             <p><strong>{film.title}</strong></p>
-            <p style={film.vote_average >= 7 ? { color: 'green' } : {}}>Note : {film.vote_average}</p>
+            <Vote vote={film.vote_average}>Note : {film.vote_average}</Vote>
             
             <p>Date de sortie : <FormatedDate date={film.release_date}/></p>
             <br />
-        </li>
+        </Li>
       ))}
-    </ul>
+    </Ul>
   );
 };
 
