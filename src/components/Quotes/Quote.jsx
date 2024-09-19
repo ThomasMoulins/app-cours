cf80e8fa81822d4197fd49a5bc7815873f8f962e
ThomasMoulins <13thomasmoulins@gmail.com>
2024-09-19 16:31:47 +0200

    TD13-Simpsons DONE

--- src/components/Quotes/Quote.jsx
+++ src/components/Quotes/Quote.jsx
@@ -1,14 +1,28 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import './Quotes.css'
+
+const Img = styled.img`
+  max-width: 5rem;
+  margin-right: 1rem;
+  ${({ direction }) => (direction === 'Right' ? 'transform: scaleX(-1);' : '')};
+`;
+
+const Li = styled.li `
+  display: flex;
+  list-style-type: none;
+  align-items: center;
+  margin: 2rem;
+`
 
 const Quote = ({quotes}) => {
   return (
     <ul>
       {quotes.map((quote, index) => (
-        <li key={index}>
-          <Link to={`/quotedetail/${quote.character}`}><img className={quote.characterDirection} src={quote.image}/></Link>
+        <Li key={index}>
+          <Link to={`/quotedetail/${quote.character}`}><Img direction={quote.characterDirection} src={quote.image}/></Link>
           <p><strong>{quote.character}:</strong> {quote.quote}</p>
-        </li>
+        </Li>
       ))}
     </ul>
   )
